29cf3db67d2352d64d70e6f468fa9ccbc10e4890
Mostafa <tefamohamed16112000@gmail.com>
2023-10-23 21:27:47 +0300

    first

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAhybBMUNtNXhqQzOjStiArZG1wYw2rfnw",
+  authDomain: "weather-e9ec0.firebaseapp.com",
+  projectId: "weather-e9ec0",
+  storageBucket: "weather-e9ec0.appspot.com",
+  messagingSenderId: "534672881594",
+  appId: "1:534672881594:web:9948742e830a05847d0f83",
+  measurementId: "G-W1F1YN7WSY"
+};
